f22d6b2d71e3a3902104f3889052ed331b7d48bc
Erwann-Kilahy <erwann.kilahy@gmail.com>
2020-06-05 15:37:46 +0200

    ajout detail de quizz séléctionné

--- quizz/urls.py
+++ quizz/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('detail/<int:idQuizz>', views.detail, name='quizz_detail'),
 
 ]
--- quizz/views.py
+++ quizz/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def detail(request, idQuizz):
+    return render(request, 'quizz_detail.html', {})
