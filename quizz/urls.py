bea84c2d189567b60417295bb56370d6babd2e2f
Damien POPIEL <damien.popiel@gmail.com>
2020-06-08 00:24:43 +0200

    Correction d'erreur d'URL

--- quizz/urls.py
+++ quizz/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('detail/<int:idQuizz>', views.detail, name='quizz_detail'),
     path('instancesQuizz/<int:idQuizz>', views.instancesQuizz, name='liste_instance_quizz'),
     path('', views.homeQuizz, name='homeQuizz'),
-    path('delete/<int:idQuizz>', views.deleteQuizz, name='deleteQuizz')
-    path('add', views.add, name='add_quizz'),
+    path('delete/<int:idQuizz>', views.deleteQuizz, name='deleteQuizz'),
+    path('add/', views.add, name='add_quizz'),
     path('add/<int:idQuizz>', views.add, name='add_quizz')
 ]
